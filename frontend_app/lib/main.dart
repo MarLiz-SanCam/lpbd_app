71061bb577f93c2812be5ff56a534d95d9adbb57
Marliz <marina.liz.santinicamarena@gmail.com>
2024-10-22 19:22:31 -0600

    DONE: Added app colors and some design to login

--- frontend_app/lib/main.dart
+++ frontend_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_app/design/app_colors.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -10,9 +11,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Lavandería APP',
+      theme: ThemeData.light().copyWith(
+        extensions: const[
+          AppColors(
+            accentuated: Color(0xFFA06CD5),
+            overlay: Color(0xFFC19EE0),
+            hint: Color(0xFFDAC3E8),),
+        ],
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: const Color(0xffA06CD5),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            backgroundColor: const Color(0xffA06CD5),
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+          ),
+        ),
       ),
       home: LoginPage(),
     );
@@ -25,11 +47,14 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //NOTE: Controladores para los campos de texto
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
-  String message = '';
-  bool _isLoading = false;
+  String message = ''; // Mensaje de respuesta del servidor
+  bool _isLoading = false; // Indicador de carga
 
+
+  //NOTE: Función para enviar los datos de login al servidor
   Future<void> login() async {
 
     setState(() {
@@ -49,13 +74,17 @@ class _LoginPageState extends State<LoginPage> {
       
     );
     if (response.statusCode == 200) {
-      // Login exitoso
+      
       final data = jsonDecode(response.body);
       setState(() {
         message = 'Login exitoso: ${data[0]['nombre_usuario']}';
       });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Login exitoso')),
+      );
+      //TODO: Importar la página de inicio del sistema
+      //Navigator.push(context, MaterialPageRoute(builder: (context) => Sistema()));
     } else {
-      // Error en el login
       setState(() {
         message = 'Error: ${response.body}';
       });
@@ -73,16 +102,21 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    var colors = Theme.of(context).extension<AppColors>()!;
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      appBar: AppBar(
+        backgroundColor: colors.accentuated,
+        title: Text('Iniciar Sesión'),),
+      body: Container(
+        color: Colors.white,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               controller: usernameController,
               decoration: InputDecoration(labelText: 'User_name'),
             ),
+            SizedBox(height: 20),
             TextField(
               controller: passwordController,
               decoration: InputDecoration(labelText: 'Password'),
@@ -91,7 +125,7 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: login,
-              child: Text('Login'),
+              child: Text('Iniciar Sesión'),
             ),
             SizedBox(height: 20),
             Text(message),
