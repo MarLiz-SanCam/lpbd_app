a0e310015e5605e20b81a66a1e4b1fcf15839cea
Marliz <marina.liz.santinicamarena@gmail.com>
2024-10-22 18:41:55 -0600

    DONE: LOGIN

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 8000);
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routes
